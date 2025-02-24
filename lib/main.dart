631847b9574ea3ba579e3b31381e12f879ee5240
bobby4fischer <jatin.singh.cd.civ22@itbhu.ac.in>
2025-02-24 16:22:24 +0530

    Updated the file with new changes

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Home());
   }
 }
 
